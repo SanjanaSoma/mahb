5cdad4a1d877a3b1a1ce3124e23ced6dba3edcd7
SanjanaSoma <122080481+SanjanaSoma@users.noreply.github.com>
2023-01-09 20:28:29 +0530

    Add files via upload

---
+++ mahb.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+from datetime import time
+from datetime import datetime
+from PIL import Image
+
+
+option = st.selectbox(    'Select an airport ',     ('Klia', 'Rgia', 'etc'))
+st.write('You selected:', option)
+st.write("<h2 style='color:black;'> Heat Map of workers at airport </h2>",
+             unsafe_allow_html=True)
+from datetime import time
+st.write("<h6 style='color:black;'> Date & time selector </h6>",
+             unsafe_allow_html=True)
+d = st.date_input("Select the date:")
+appointment = st.slider(
+    "Select the time range:",
+    value=(time(11, 30), time(12, 45)))
+
+
+col1, col2, col3 = st.columns([2,6,1])
+
+with col1:
+    st.write("")
+
+with col2:
+    img = Image.open("map2.jpeg")
+    new_image = img
+    st.image(new_image)
+
+with col3:
+    st.write("")
+
+
+
+st.write("<h2 style='color:black;'> Real & Estimated wait times </h2>",
+             unsafe_allow_html=True)
+
+img2 = Image.open("chart.jpeg")
+new_image1 = img2
+st.image(new_image1)
